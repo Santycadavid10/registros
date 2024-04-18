19b73b93cf23861e11992d38ba7ea040270b8779
nada <nada@gmail.com>
2024-04-18 13:58:23 -0500

    sssssss

--- Models/registros.cs
+++ Models/registros.cs
@@ -8,4 +8,15 @@ namespace Registros.Models
 
 
   }
+
+    public class Registro
+  {
+    public int id { get; set; }
+    
+    public string? documento_usuario { get; set; }
+    public DateTime 	hora_ingreso { get; set; }
+    public DateTime 	hora_salida  { get; set; }
+    
+
+  }
 }
